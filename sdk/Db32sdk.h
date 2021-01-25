7c2eee6a74a55a3c7e06d9c62e003f4918cddf76
tankaishuai <tankaishuai@126.com>
2021-01-25 12:06:53 +0800

    更新至 V70

--- sdk/Db32sdk.h
+++ sdk/Db32sdk.h
@@ -9,6 +9,12 @@ struct IDb32Fetch
     const VARIANT *varVal) = 0;
 };
 
+typedef bool (* OnFetch_t)(
+  long lRows, long lCols, long lNumCols,
+  const void *varKey, bool bIsUnicodeKey, 
+  const VARIANT *varVal,
+  void *pUser);
+
 struct IDb32;
 
 #define DB32_SCHEMA_DEFAULT  (-1L)
@@ -36,7 +42,22 @@ struct IDb32
 };
 
 IDb32 * win32db_open(LPCWSTR szConn, LPCWSTR szUser, LPCWSTR szPassword);
+IDb32 * win32db_open_v2(LPCWSTR szConn, LPCWSTR szUser, LPCWSTR szPassword);
+bool    win32db_schema(IDb32 *pWrapper, long lType, LPCWSTR szSql);
+bool    win32db_exec(IDb32 *pWrapper, LPCWSTR szSql, long lFlags);
+bool    win32db_exec_v2(IDb32 *pWrapper, LPCWSTR szSql, long lMaxRows, long lFlags, OnFetch_t pfnFetch, void *pUser);
+bool    win32db_fetch(IDb32 *pWrapper, long lMaxRows, long lFlags, OnFetch_t pfnFetch, void *pUser);
+void    win32db_close(IDb32 *pWrapper);
+long    win32db_get_ddl(IDb32 *pWrapper, LPCWSTR szTbl, LPWSTR szText, long cchText);
+void *  win32db_raw_handle(IDb32 *pWrapper);
+LPCWSTR win32db_get_type(IDb32 *pWrapper);
 
 void *  win32db_blob_ptr(const VARIANT *varVal);
 int     win32db_blob_bytes(const VARIANT *varVal);
 bool    win32db_blob_is_valid(const VARIANT *varVal);
+
+typedef IDb32 * (* win32db_raw_create_by_conn_t)(__inout LPCWSTR &szConn);
+IDb32 * win32db_raw_create_by_conn(__inout LPCWSTR &szConn);
+LRESULT APIENTRY win32db_open_guiA(HWND hParentWnd, HINSTANCE, LPCSTR szConn, UINT nCmdShow);
+LRESULT APIENTRY win32db_open_guiW(HWND hParentWnd, HINSTANCE, LPCWSTR szConn, UINT nCmdShow);
+INT_PTR win32db_open_gui_core(HWND hParentWnd, LPCWSTR szConn, bool bAllowSort, DLGPROC pfnWndProc);
